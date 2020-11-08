570ee41737ab3ee65824f9fad65c8e294f955476
WeiwenXu21 <wx37523@uga.edu>
2020-11-07 22:07:34 -0500

    Client-side

---
+++ src/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faBowlingBall } from '@fortawesome/free-solid-svg-icons'
+// <FontAwesomeIcon icon={faBowlingBall} size='5x' color='#3B5998' />
+export default () =>
+  <div className='spinner fadein'>
+    <div className='App'>
+      <div className='App-header'>
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    </div>
+  </div>
